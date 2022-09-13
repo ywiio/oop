68fcb71b8fe0d32ca09eac374d598a5df1bd5d31
ywiio <maschkantseva.zlata@yandex.ru>
2022-09-13 13:58:58 +0300

    tc

--- first/first/obj/Debug/net6.0/first.AssemblyInfo.cs
+++ first/first/obj/Debug/net6.0/first.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ffirst")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ffirst")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ffirst")]
+[assembly: System.Reflection.AssemblyProductAttribute("first")]
+[assembly: System.Reflection.AssemblyTitleAttribute("first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
